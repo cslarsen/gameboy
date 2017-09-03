3a555f6e558a30493babc723eb01dc94d69d5a2b
Christian Stigen Larsen <csl@csl.name>
2017-09-03 14:44:15 +0200

    Shows cartridge type

--- gameboy/cartridge.py
+++ gameboy/cartridge.py
@@ -26,8 +26,9 @@ class Cartridge(object):
         return sum(map(len, self.rom_bank))
 
     def __repr__(self):
-        return "<Cartridge: title=%r type=%r bytes=$%x ROM size=$%x, RAM size=$%x>" % (
-                self.title, self.type, len(self), self.rom_size, self.ram_size)
+        return "<Cartridge: title=%r color=%r sgb=%r type=%r bytes=$%x ROM banks=%d, RAM banks=%s>" % (
+                self.title, self.color, self.super_gameboy, self.type,
+                len(self), self.rom_banks, self.ram_banks)
 
     @property
     def title(self):
@@ -39,21 +40,82 @@ class Cartridge(object):
         return s
 
     @property
-    def rom_size(self):
-        return self.rom_bank[0][0x148]
+    def color(self):
+        return self.rom_bank[0][0x143] == 0x80
 
     @property
-    def ram_size(self):
-        return self.rom_bank[0][0x149]
+    def rom_banks(self):
+        size = self.rom_bank[0][0x148]
+        return {
+            0x0: 2,
+            0x1: 4,
+            0x2: 8,
+            0x3: 16,
+            0x4: 32,
+            0x5: 64,
+            0x6: 128,
+            0x52: 72,
+            0x53: 80,
+            0x54: 96,
+        }.get(size)
 
     @property
-    def type(self):
+    def ram_banks(self):
+        return {
+            0: None,
+            1: 1,
+            2: 1,
+            3: 4,
+            4: 16,
+        }.get(self.rom_bank[0][0x149])
+
+    @property
+    def destination_code(self):
+        return {
+            0: "Japanese",
+            1: "Non-Japanese",
+        }.get(self.rom_bank[0][0x14a])
+
+    @property
+    def super_gameboy(self):
         """Whether this is a classic GameBoy or Super GameBoy cartridge."""
         t = self.rom_bank[0][0x146]
 
         if t == 0x00:
-            return "GameBoy"
+            return False # Gameboy
         if t == 0x03:
-            return "Super GameBoy"
+            return True # Super Gameboy functions
 
         raise RuntimeError("Invalid cartridge type %0.2x" % t)
+
+    @property
+    def type(self):
+        """Cartridge type."""
+        return {
+            0x00: "ROM only",
+            0x01: "ROM + MBC1",
+            0x02: "ROM + MBC1 + RAM",
+            0x03: "ROM + MBC1 + RAM + BATTERY",
+            0x05: "ROM + MBC2",
+            0x06: "ROM + MBC2 + BATTERY",
+            0x08: "ROM + RAM",
+            0x09: "ROM + RAM + BATTERY",
+            0x0b: "ROM + MMM01",
+            0x0c: "ROM + MMM01 + SRAM",
+            0x0d: "ROM + MMM01 + SRAM + BATTERY",
+            0x0f: "ROM + MBC3 + TIMER + BATTERY",
+            0x10: "ROM + MBC3 + TIMER + RAM + BATTERY",
+            0x11: "ROM + MBC3",
+            0x12: "ROM + MBC3 + RAM",
+            0x13: "ROM + MBC3 + RAM + BATTERY",
+            0x19: "ROM + MBC5",
+            0x1a: "ROM + MBC5 + RAM",
+            0x1b: "ROM + MBC5 + RAM + BATTERY",
+            0x1c: "ROM + MBC5 + RUMBLE",
+            0x1d: "ROM + MBC5 + RUMBLE + SRAM",
+            0x1e: "ROM + MBC5 + RUMBLE + SRAM + BATTERY",
+            0x1f: "Pocket Camera",
+            0xfd: "Bandai TAMA5",
+            0xfe: "Hudson HuC-3",
+            0xff: "Hudson HuC-1",
+        }.get(self.rom_bank[0][0x147], "Unknown")
