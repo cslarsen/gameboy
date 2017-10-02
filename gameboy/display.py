5d7bcdc47e12bf2329a03803182431dc5f4df552
Christian Stigen Larsen <csl@csl.name>
2017-10-02 23:27:33 +0200

    Toggles tile window on T

--- gameboy/display.py
+++ gameboy/display.py
@@ -26,11 +26,19 @@ class HostDisplay(object):
                 flags = sdl2.SDL_RENDERER_ACCELERATED |
                         sdl2.SDL_RENDERER_PRESENTVSYNC)
 
-    def pump_events(self):
+        self.shown = False
+
+    def pump_events(self, display):
         events = sdl2.ext.get_events()
         for event in events:
             if event.type == sdl2.SDL_QUIT:
                 raise StopIteration("SDL quit")
+            elif event.type == sdl2.SDL_KEYDOWN:
+                if event.key.keysym.sym == ord('t'):
+                    if display.tile_window.shown:
+                        display.tile_window.hide()
+                    else:
+                        display.tile_window.show()
 
     def put(self, x, y, color):
         self.renderer.draw_point((x,y), color)
@@ -39,7 +47,12 @@ class HostDisplay(object):
         self.renderer.draw_line((x1, y1, x2, y2), color)
 
     def show(self):
+        self.shown = True
         self.window.show()
+
+    def hide(self):
+        self.shown = False
+        self.window.hide()
 
     def update(self):
         self.renderer.present()
@@ -96,7 +109,6 @@ class Display(object):
             self.tile_window = NoDisplay()
 
         self.window.show()
-        self.tile_window.show()
 
         self.window.clear(0x474741)
         self.tile_window.clear(0x474741)
@@ -269,7 +281,7 @@ class Display(object):
     def step(self):
         """Renders one scanline."""
         if not self.screen_on:
-            self.window.pump_events()
+            self.window.pump_events(self)
             return
 
         if self.background_display:
@@ -284,7 +296,7 @@ class Display(object):
             self.flush_pixels()
             self.flush_tile_pixels()
             self.show_viewport()
-            self.window.pump_events()
+            self.window.pump_events(self)
             self.window.update()
             self.tile_window.update()
             self.calc_fps()
